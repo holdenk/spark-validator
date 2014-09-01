91c94b588c8d4b1e0787e088a11e9282738b9717
Holden Karau <holden@pigscanfly.ca>
2014-09-01 11:57:57 -0700

    Add a abstract class for validation rules which don't use historic data

--- src/main/scala/com/holdenkarau/spark-validator/ValidationRule.scala
+++ src/main/scala/com/holdenkarau/spark-validator/ValidationRule.scala
@@ -8,3 +8,11 @@ import com.holdenkarau.spark_validator.HistoricDataProtos.HistoricData
 abstract class ValidationRule {
   def validate(historicData: List[HistoricData], vl: ValidationListener, counters: scala.collection.Map[String, Numeric[_]]): Boolean
 }
+
+abstract class AbsoluteValidationRule extends ValidationRule {
+  override def validate(historicData: List[HistoricData], vl: ValidationListener, counters: scala.collection.Map[String, Numeric[_]]): Boolean = {
+    validate(vl, counters)
+  }
+  def validate(vl: ValidationListener, counters: scala.collection.Map[String, Numeric[_]]): Boolean
+
+}
