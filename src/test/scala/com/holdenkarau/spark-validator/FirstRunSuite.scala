e6f9e7bd344cf2c8c3f3b1b269e39e65588ac941
Holden Karau <holden@pigscanfly.ca>
2014-09-10 10:23:22 -0700

    Add an empty test

---
+++ src/test/scala/com/holdenkarau/spark-validator/FirstRunSuite.scala
@@ -0,0 +1,14 @@
+/*
+ * Verifys that the Spark Validator functions at least somewhat on first run
+ */
+
+package com.holdenkarau.spark_validator
+
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
+class SparkContextSchedulerCreationSuite
+  extends FunSuite {
+
+}
