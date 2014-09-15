6347c5ed9c263fa4f5ed105e396978874149eb8a
Holden Karau <holden@pigscanfly.ca>
2014-09-14 17:57:02 -0700

    Add a first test, it fails of course

--- src/test/scala/com/holdenkarau/spark-validator/FirstRunSuite.scala
+++ src/test/scala/com/holdenkarau/spark-validator/FirstRunSuite.scala
@@ -4,11 +4,20 @@
 
 package com.holdenkarau.spark_validator
 
-import org.scalatest.{BeforeAndAfterEach, FunSuite}
+import org.scalatest.{Assertions, BeforeAndAfterEach, FunSuite}
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
+import java.nio.file.Files;
 
 class SparkContextSchedulerCreationSuite
   extends FunSuite {
-
+  // TODO(holden): factor out a bunch of stuff but lets add a first test as a starting point
+  test("null validation test") {
+    val sc = new SparkContext("local", "test")
+    val tempPath = Files.createTempDirectory(null).toString()
+    val vc = new ValidationConf(tempPath, "1", true, List[ValidationRule]())
+    val v = new Validation(sc, vc)
+    sc.stop()
+    assert(v.validate() === true)
+  }
 }
