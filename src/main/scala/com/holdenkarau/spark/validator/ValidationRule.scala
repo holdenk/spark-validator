3aa7bc32e36990f06035465bfbea5d265669d7a6
Mahmoud Hanafy <mahmoud.hanafy@badrit.com>
2016-03-26 07:33:10 +0200

    Compare value with average by absolute difference

--- src/main/scala/com/holdenkarau/spark/validator/ValidationRule.scala
+++ src/main/scala/com/holdenkarau/spark/validator/ValidationRule.scala
@@ -44,11 +44,11 @@ case class AvgRule(counterName: String,
         }
       }
       case head :: tail => {
-        // TODO what if tail = Nill
         val avg = tail.foldLeft((head.toDouble, 1.0))((r: (Double, Double), c: Long) =>
-          ((r._1 + (c.toDouble/r._2)) * r._2 / (r._2+1), r._2+1))._1
-        val value = current.counters.get(counterName).get.asInstanceOf[Long]
-        if ((avg-maxDiff < value) && (value < avg+maxDiff)) {
+          ((r._1 + (c.toDouble / r._2)) * r._2 / (r._2 + 1), r._2 + 1))._1
+
+        val value = current.counters.get(counterName).get
+        if (Math.abs(value - avg)<= maxDiff) {
           None
         } else {
           Some(s"Value $value for counter $counterName was not in the range of " +
